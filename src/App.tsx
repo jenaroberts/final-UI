8e4cdee88d25d73f8ce1fcffb64d778988880248
Jena Roberts <jenaroberts@Jenas-Air.attlocal.net>
2022-06-06 21:28:16 -0400

    setting up react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@emotion/react";
+import { Home } from "@mui/icons-material";
+import { createTheme, CssBaseline } from "@mui/material";
+import "./scss/app.scss";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `fantasy`,
+  },
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#82b3c9",
+    },
+    primary: {
+      main: "#rgb(11, 11, 65)",
+    },
+    secondary: {
+      main: "#ffb300",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <h1>home page</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Input } from "@mui/material";
+
+export const Home = () => {
+  return <Input aria-label="email" />;
+};
